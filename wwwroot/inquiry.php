00d26fb9507a0411329c0ad9f370a9e7f2b6f218
tuang <tuang@example.com>
2017-05-23 17:27:37 +0900

    DB insert

--- wwwroot/inquiry.php
+++ wwwroot/inquiry.php
@@ -1,58 +1,82 @@
 <?php
-
+// inquiry.php
+//
 ob_start();
 session_start();
-
+// 確認
 //var_dump($_SESSION);
-
-//入力内容を取得
-//$input = $_SESSION['buffer']['input'];
+// 入力内容を取得
+//$input = $_SESSION['buffer']['input'] ?? []; // PHP 7.0以降ならこっち
 if (true === isset($_SESSION['buffer']['input'])) {
-	$input = $_SESSION['buffer']['input'];
-}else {
-	$input = array();
+    $input = $_SESSION['buffer']['input'];
+} else {
+    //$input = []; // PHP 5.4以降ならこっちでもよい
+    $input = array();
 }
-
-//error内容を取得
-//$error_detail = $_SESSION['buffer']['error_detail'];
-if (true === isset($_SESSION['buffer']['error_deatil'])) {
-	$error_detail = $_SESSION['buffer']['error_detail'];
-}else {
-	$error_detail = array();
+// エラー内容を取得
+//$error_detail = $_SESSION['buffer']['error_detail'] ?? [];
+if (true === isset($_SESSION['buffer']['error_detail'])) {
+    $error_detail = $_SESSION['buffer']['error_detail'];
+} else {
+    //$error_detail = []; // PHP 5.4以降ならこっちでもよい
+    $error_detail = array();
 }
+//var_dump($error_detail);
 
-//XSS対策関数
-function h($a) {
-	return htmlspecialchars($a, ENT_QUOTES);
+//CSRF トークンを作成
+// XXX php7前提
+$csrf_token = hash('sha512',random_bytes(128));
+//var_dump($csrf_token);
+//CSRFトークンは10個まで
+while (10 <= count(@$_SESSION['csrf_token'])) {
+	array_shift($_SESSION['csrf_token']);
+}
+//CSRF トークンをSESSIONに入れておく：時間付き
+$_SESSION['csrf_token'][$csrf_token] = time();
+// XSS対策用関数
+function h($s) {
+    return htmlspecialchars($s, ENT_QUOTES);
 }
 ?>
 <html>
+
 <body>
 <?php
-	if (0 < count($error_detail)) {
-		echo '<div style="color:red;">エラーがありました。</div>';
-	}
+  if (0 < count($error_detail)) {
+    echo '<div style="color: red;">エラーがあります</div>';
+  }
 ?>
 
 <?php
-	if (isset($error_detail['error_must_email'])) {
-		echo '<div style="color:red;">メアドが必要です。</div>';
-	}
+  // error_must_email
+  if (isset($error_detail['error_must_email'])) {
+    echo '<div style="color: red;">メアドは必須です。</div>';
+  }
+  if (isset($error_detail['error_csrf_token'])) {
+    echo '<div style="color: red;">トークンエラー</div>';
+  }
+/*
+error_must_body
+error_format_email
+error_format_birthday
+*/
 ?>
-	<form action="./inquiry_fin.php" method="post">
-		emailアドレス（＊）：<input type="text" name="email"
-			value="<?php echo h(@$input['email']); ?>"><br>
+  <form action="./inquiry_fin.php" method="post">
+    emailアドレス(*):<input type="text" name="email"
+        value="<?php echo h((string)@$input['email']); ?>"><br>
 
-		名前：<input type="text" name="name" 
-			value="<?php echo h(@$input['name']); ?>"><br>
+    名前:<input type="text" name="name"
+        value="<?php echo h((string)@$input['name']); ?>"><br>
 
-		誕生日：<input type="text" name="birthday"
-			value="<?php echo h(@$input['birthday']); ?>"><br>
+    誕生日:<input type="text" name="birthday"
+        value="<?php echo h((string)@$input['birthday']); ?>"><br>
 
-		問い合わせ内容：<textarea name="body"
-			value="<?php echo h(@$input['body']); ?>"></textarea><br>
-		<button>問い合わせる</button>
-	</form>
+    問い合わせ内容<textarea name="body">
+<?php echo h((string)@$input['body']); ?></textarea><br>
+		
+		<input type="hidden" name="csrf_token" 
+					value="<?php echo h($csrf_token); ?>">
+    <button>問い合わせる</button>
+  </form>
 </body>
 </html>
-
--- wwwroot/inquiry_fin.php
+++ wwwroot/inquiry_fin.php
@@ -4,6 +4,9 @@
 ob_start();
 session_start();
 
+//
+require_once(__DIR__. '/dbh.php');
+
 $params = [
 	'email','name','birthday','body'
 ];
@@ -12,10 +15,31 @@ foreach($params as $p) {
 	$input_data[$p] = (string)@$_POST[$p];
 }
 
-var_dump($input_data);
+//var_dump($input_data);
 
+//validate（情報正しい？）
 $error_detail = [];
 
+//csrfチェック
+//tokenの存在確認（check exists）
+$posted_token = $_POST['csrf_token'];
+if (false == isset($_SESSION['csrf_token'][$posted_token])) {
+	//error detail
+	$error_detail['error_csrf_token'] = true;
+}else {
+		$ttl = $_SESSION['csrf_token'][$posted_token];
+		//寿命確認
+		if (time() >= $ttl + 60) {
+			//token作成から60秒以上経過しているのでNG
+			$error_detail['error_csrf_timeover']=time() . " >= {$ttl}";
+		}
+		unset($_SESSION['csrf_token'][$posted_token]);
+	}
+
+//tokenの寿命確認 (check life)
+
+
+//必要チェック
 $must_params = ['email','body'];
 foreach ($must_params as $p) {
 	if ('' === $input_data[$p]) {
@@ -45,7 +69,39 @@ if ([] != $error_detail) {
 exit;
 }
 //ダミー
-echo 'データのvalidatateはOKでした！！';
-//入力された情報をDBにINSERT
+//echo 'データのvalidatateはOKでした！！';
 
+//入力された情報をDBにINSERT
+//DBハンドルを取得
+$dbh = get_dbh();
+
+//ＳＱＬ文（準備された文）を作成
+$sql = 'INSERT INTO inquirys(email, inquiry_body, name, birthday)
+	VALUES(:email, :inquiry_body, :name, :birthday);';
+$pre = $dbh->prepare($sql);
+
+//データーをバインド
+$pre->bindValue(':email',$input_data['email']);
+$pre->bindValue(':inquiry_body',$input_data['body']);
+$pre->bindValue(':name',$input_data['name']);
+$pre->bindValue(':birthday',$input_data['birthday']);
+//ＳＱＬ実行
+$r = $pre->execute();
+if (false == $r) {
+	echo "データー取得できませんでした";
+	exit;
+}
+//var_dump($r);
 //ありがとうページ
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Thank you Man
+	</title>
+</head>
+<body>
+入力ありがとうございます。
+</body>
+</html>
